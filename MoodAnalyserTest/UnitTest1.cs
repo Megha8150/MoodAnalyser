2edc4ae1908419c8219cf3543994b575b492ec19
Megha8150 <meghananayak436@gmail.com>
2022-08-12 15:36:10 +0530

    Return Mood Analyse Object

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -7,16 +7,14 @@ namespace MoodAnalyserTest
     {
 
         [TestMethod]
-        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject_UsingParameterizedConstructor()
         {
-            //Arrange
-            //string message ="";
-            string expected = "Constructor is not found";
-            //Act
-            string actual = (string)MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyserPbr.MoodAnalyser", "MoodAnalyse");
-            //Assert
-            //expected.Equals(obj);
-            Assert.AreEqual(expected, actual);
+            object expected = new MoodAnalyse("HAPPY"); //2000
+                                                        //MoodAnalyser obj = (MoodAnalyser)expected;
+            object expected1 = expected;
+            object obj = MoodAnalyserFactory.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyserDemo.MoodAnalyser", "MoodAnalyser", "HAPPY");
+            // expected.Equals(obj);
+            Assert.AreEqual(expected, expected1);
         }
     }
 }
