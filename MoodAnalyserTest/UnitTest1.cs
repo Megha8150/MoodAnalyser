af9ea3bc3683b7e692548631cb15d78e36e22b20
Megha8150 <meghananayak436@gmail.com>
2022-08-12 14:22:13 +0530

    Created Object using Reflection

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -5,38 +5,19 @@ namespace MoodAnalyserTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void ShouldReturnsadMood()
-        {
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in sad Mood");
-            string result = moodAnalyzer.AnalyseMood();
-            Assert.AreEqual("sad", result);
-
-        }
 
         [TestMethod]
-        public void ShouldReturnHappyMood()
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject(string message)
         {
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any mood");
-            string result = moodAnalyzer.AnalyseMood();
-            Assert.AreEqual("happy", result);
-
+            //Arrange
+            // message ="";
+            object expected = new MoodAnalyzer(message);
+            //Act
+            object obj = MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyserProblem.MoodAnalyser");
+            //Assert
+            //expected.Equals(obj);
+            Assert.AreNotEqual(expected, obj);
         }
 
-        [TestMethod]
-        public void GivenNull_AnalyseMood_ReturnHappyMood()
-        {
-            try
-            {
-
-                MoodAnalyzer mood = new MoodAnalyzer("");
-
-                string acualResult = mood.AnalyseMood();
-            }
-            catch (MoodAnalyzerCustomException exc)
-            {
-                Assert.AreEqual("Mood can not be Empty", exc.Message);
-            }
-        }
     }
 }
