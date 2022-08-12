34e63b86930d024d13aad6d7a753fe5a169c0d3a
Megha8150 <meghananayak436@gmail.com>
2022-08-12 15:04:51 +0530

    passed empty mood and threw exception

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -5,16 +5,12 @@ namespace MoodAnalyserTest
     [TestClass]
     public class UnitTest1
     {
-
         [TestMethod]
-        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject(string message)
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
         {
-            //Arrange
-            // message ="";
-            object expected = new MoodAnalyzer(message);
-            //Act
-            object obj = MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyserProblem.MoodAnalyser");
-            //Assert
+            string message = null;
+            object expected = new MoodAnalyse(message);
+            object obj = MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyserDemo.MoodAnalyser", "MoodAnalyser");
             //expected.Equals(obj);
             Assert.AreNotEqual(expected, obj);
         }
--- MoodAnalyzerProblem/MoodAnalyzerFactory.cs
+++ MoodAnalyzerProblem/MoodAnalyzerFactory.cs
@@ -4,17 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace MoodAnalyzerProblem
 {
-        public class MoodAnalyserFactory
+    public class MoodAnalyserFactory
+    {
+        public static object CreateMoodAnalyser(string className, string constructorName)
         {
-            public static object CreateMoodAnalyser(string className)
+            string pattern = @"." + constructorName + "$";//.MoodAnalyser$
+            Match result = Regex.Match(className, pattern);
+            try
             {
-                Assembly assembly = Assembly.GetExecutingAssembly();
-                Type moodAnalysType = assembly.GetType(className);
-                return Activator.CreateInstance(moodAnalysType);
+                if (result.Success)
+                {
+                    try
+                    {
+                        Assembly executing = Assembly.GetExecutingAssembly();
+                        Type moodAnalyseType = executing.GetType(className);
+                        return Activator.CreateInstance(moodAnalyseType);
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        Console.WriteLine("Your input is not valid");
+                        throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+                    }
+                }
+                else
+                {
+                    throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+                }
             }
+            catch (MoodAnalyzerCustomException ex)
+            {
+                return ex.Message;
+            }
+            return null;
         }
-    
+    }
+
 }
